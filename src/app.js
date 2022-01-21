bb56f9de58d390979a8c9cbd2fa0da4d42fa047c
Sergey Kudelin <kudelin.work@bk.ru>
2022-01-22 00:09:45 +0300

    added: admin taskfield, admin add new user

--- src/app.js
+++ src/app.js
@@ -2,12 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/style.css";
 import taskFieldTemplate from "./templates/taskField.html";
 import noAccessTemplate from "./templates/noAccess.html";
+import taskFieldAdminTemplate from "./templates/taskFieldAdmin.html";
 import { User } from "./models/User";
-import { generateTestUser } from "./utils";
-import { generateTestAdmin } from "./utils";
+import { generateAdmin } from "./utils";
 import { State } from "./state";
 import { authUser } from "./services/auth";
-import { Admin } from "./models/Admin";
+import { addUser } from "./utils";
 
 export const appState = new State();
 
@@ -15,8 +15,7 @@ const loginForm = document.querySelector("#app-login-form");
 
 //debugger;
 
-generateTestUser(User);
-generateTestAdmin(Admin);
+generateAdmin(User);
 
 loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -24,9 +23,14 @@ loginForm.addEventListener("submit", function (e) {
   const login = formData.get("login");
   const password = formData.get("password");
 
-  let fieldHTMLContent = authUser(login, password)
-    ? taskFieldTemplate
-    : noAccessTemplate;
+  //debugger;
 
-  document.querySelector("#content").innerHTML = fieldHTMLContent;
+  if (authUser(login, password) === 'admin') {
+    document.querySelector("#content").innerHTML = taskFieldAdminTemplate;
+    addUser();
+  } else if (authUser(login, password) === true) {
+    document.querySelector("#content").innerHTML = taskFieldTemplate;
+  } else {
+    document.querySelector("#content").innerHTML = noAccessTemplate;
+  };
 });
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import { User } from "./models/User";
+
 export const getFromStorage = function (key) {
   return JSON.parse(localStorage.getItem(key) || "[]");
 };
@@ -8,13 +10,24 @@ export const addToStorage = function (obj, key) {
   localStorage.setItem(key, JSON.stringify(storageData));
 };
 
-export const generateTestUser = function (User) {
+export const generateAdmin = function (User) {
   localStorage.clear();
-  const testUser = new User("test", "qwerty123");
-  User.save(testUser);
+  const admin = new User("admin", "admin");
+  admin.admin = true;
+  User.save(admin);
 };
 
-export const generateTestAdmin = function (Admin) {
-  const testAdmin = new Admin("adminlog", "adminpas");
-  Admin.save(testAdmin);
+export const addUser = function () {
+  const addUserForm = document.querySelector('#app-add-user-form');
+
+  addUserForm.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    debugger;
+    const formData = new FormData(addUserForm);
+    const login = formData.get("login");
+    const password = formData.get("password");
+    const newUser = new User(login, password);
+    User.save(newUser);
+  });
 };
+  

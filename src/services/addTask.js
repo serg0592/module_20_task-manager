cabb9c08db74a7147083424f44739eec498b9d67
Sergey Kudelin <kudelin.work@bk.ru>
2022-01-24 22:15:44 +0300

    CSS: add task, kanban; Added: add task window

--- src/services/addTask.js
+++ src/services/addTask.js
@@ -1,3 +1,25 @@
 export const addTask = function () {
     const addTaskBtn = document.querySelector(".app-btn-add-task");
+
+    addTaskBtn.addEventListener('click', function () {
+        const kanban = document.querySelector(".kanban");
+
+        const newTask = document.createElement('div');
+        newTask.className = "new-task-window";
+
+        const newTaskP1 = document.createElement('p');
+        newTaskP1.innerHTML = 'Название задачи<br>';
+
+        const newTaskP2 = document.createElement('p');
+        newTaskP2.innerHTML = "Описание задачи<Br>";
+
+        const newTaskTextArea = document.createElement('textarea');
+        newTaskTextArea.className = "new-task-text";
+
+        kanban.prepend(newTask);
+        newTask.appendChild(newTaskP2);
+        newTaskP2.appendChild(newTaskTextArea);
+
+        const toDoList = document.querySelector(".todo-list");        
+    });
 }

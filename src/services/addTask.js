c0f8f1e6fcc3c31c073d057d4dbe71c9257ddec9
Sergey Kudelin <kudelin.work@bk.ru>
2022-01-26 21:22:58 +0300

    Updated: kanban-todo, task-window, Add card btn; Added: new task to todo-list

--- src/services/addTask.js
+++ src/services/addTask.js
@@ -1,46 +1,72 @@
 export const addTask = function () {
     const toDoList = document.querySelector(".todo-list");   
     const addTaskBtn = document.querySelector(".app-btn-add-task");
+    const kanban = document.querySelector(".kanban");
 
     addTaskBtn.addEventListener('click', function () {
-        const kanban = document.querySelector(".kanban");
-
         const newTask = document.createElement('div');
         newTask.className = "new-task-window";
 
-        const newTaskP1 = document.createElement('p');
-        newTaskP1.innerHTML = 'Название задачи<br>';
+        const newTaskForm = document.createElement('form');
 
-        const newTaskTextArea1 = document.createElement('textarea');
-        newTaskTextArea1.className = "new-task-name";
-        newTaskTextArea1.rows = '1';
+        const newTaskName = document.createElement('p');
+        newTaskName.innerHTML = 'Название задачи<br>';
+
+        const newTaskNameText = document.createElement('input');
+        newTaskNameText.className = "new-task-name";
+        newTaskNameText.rows = '1';
+        newTaskNameText.cols = '80';
+        newTaskNameText.maxLength = '60';
 
         const newTaskCloseBtn = document.createElement('button');
         newTaskCloseBtn.className = 'btn app-btn-close';
         newTaskCloseBtn.innerHTML = 'X';
 
-        const newTaskP2 = document.createElement('p');
-        newTaskP2.innerHTML = "Описание задачи<Br>";
+        const newTaskDescr = document.createElement('p');
+        newTaskDescr.innerHTML = "Описание задачи<Br>";
 
-        const newTaskTextArea2 = document.createElement('textarea');
-        newTaskTextArea2.className = "new-task-text";
-        newTaskTextArea2.rows = '8';
+        const newTaskDescrText = document.createElement('textarea');
+        newTaskDescrText.className = "new-task-text";
+        newTaskDescrText.rows = '8';
 
         const newTaskSubmitBtn = document.createElement('button');
         newTaskSubmitBtn.className = 'btn btn-outline-info app-btn-task-submit';
+        newTaskSubmitBtn.type = 'submit';
         newTaskSubmitBtn.innerHTML = 'Submit';
 
         kanban.prepend(newTask);
+        newTask.appendChild(newTaskForm);
         newTask.appendChild(newTaskCloseBtn);
-        newTask.appendChild(newTaskP1);
-        newTaskP1.appendChild(newTaskTextArea1);
-        newTask.appendChild(newTaskP2);
-        newTaskP2.appendChild(newTaskTextArea2);
-        newTask.appendChild(newTaskSubmitBtn);     
+        newTaskForm.appendChild(newTaskName);
+        newTaskName.appendChild(newTaskNameText);
+        newTaskForm.appendChild(newTaskDescr);
+        newTaskDescr.appendChild(newTaskDescrText);
+        newTaskForm.appendChild(newTaskSubmitBtn);
+
+        newTaskNameText.focus();
+
+        newTaskCloseBtn.addEventListener('click', function () {
+            kanban.removeChild(newTask);
+        });
+
+        newTaskForm.addEventListener('submit', function (e) {
+            e.preventDefault();            
+            if (newTaskNameText.value === '') {
+                alert('Введите название задачи');
+            } else {
+                const toDoNode = document.createElement('div');
+                toDoNode.className = 'todo-node';
+
+                const toDoNodeName = document.createElement('p');
+                toDoNodeName.className = 'todo-node-name';
+
+                toDoNodeName.innerHTML = newTaskNameText.value;
+
+                toDoList.appendChild(toDoNode);
+                toDoNode.appendChild(toDoNodeName);
+
+                kanban.removeChild(newTask);
+            };
+        });
     });
-
-    const newTaskCloseBtn = document.querySelector('.app-btn-close');
-    const newTaskSubmitBtn = document.querySelector('.app-btn-task-submit');
-
-    
 }

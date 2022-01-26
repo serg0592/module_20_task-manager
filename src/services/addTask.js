440f979e06cfd1ace2f0d5abbe151817324df654
Sergey Kudelin <kudelin.work@bk.ru>
2022-01-26 09:56:25 +0300

    Task window: close btn added

--- src/services/addTask.js
+++ src/services/addTask.js
@@ -7,23 +7,15 @@ export const addTask = function () {
         const newTask = document.createElement('div');
         newTask.className = "new-task-window";
 
-        newTask.style.position = 'absolute';
-        newTask.style.borderWidth = '2px';
-        newTask.style.borderStyle = 'solid';
-        newTask.style.borderColor = '#0c8ea8';
-        newTask.style.borderRadius = '10px';
-        newTask.style.backgroundColor = 'white';
-        newTask.style.marginTop = '40px';
-        newTask.style.marginBottom = '40px';
-        newTask.style.marginLeft = '100px';
-        newTask.style.marginRight = '100px';
-        newTask.style.zIndex = '999999';
-
         const newTaskP1 = document.createElement('p');
         newTaskP1.innerHTML = 'Название задачи<br>';
 
         const newTaskTextArea1 = document.createElement('textarea');
         newTaskTextArea1.className = "new-task-name";
+
+        const newTaskCloseBtn = document.createElement('button');
+        newTaskCloseBtn.className = 'btn app-btn-close';
+        newTaskCloseBtn.innerHTML = 'X';
 
         const newTaskP2 = document.createElement('p');
         newTaskP2.innerHTML = "Описание задачи<Br>";
@@ -32,6 +24,7 @@ export const addTask = function () {
         newTaskTextArea2.className = "new-task-text";
 
         kanban.prepend(newTask);
+        newTask.appendChild(newTaskCloseBtn);
         newTask.appendChild(newTaskP1);
         newTaskP1.appendChild(newTaskTextArea1);
         newTask.appendChild(newTaskP2);

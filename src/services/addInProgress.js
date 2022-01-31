dd7f9f97d797bfe9a5dbc220f19fb0d8d162adec
Sergey Kudelin <kudelin.work@bk.ru>
2022-01-31 16:10:32 +0300

    Continuing previous

--- src/services/addInProgress.js
+++ src/services/addInProgress.js
@@ -1,46 +1,40 @@
 import { chooseAddCardItem } from './chooseAddCardItem';
 
 export const addInProgress = function () {
-    const addInProgress = document.querySelector('.app-add-card-in-progress');
+    const addCardInProgress = document.querySelector('.app-add-card-in-progress');
     const addCardInProgressDD = document.querySelector('.app-add-in-progress-dropdown');
+    let open;
     let addCardItems = document.querySelector('.app-add-in-progress-dropdown').childNodes;
-    let open;addCardItems = document.querySelector('.app-add-in-progress-dropdown').childNodes;
 
-    addInProgress.addEventListener('click', function () {
-        const addInProgress = document.querySelector('.app-add-card-in-progress');
-        const addCardInProgressDD = document.querySelector('.app-add-in-progress-dropdown');
-        let addCardItems = document.querySelector('.app-add-in-progress-dropdown').childNodes;
-        let open;
+    addCardInProgress.addEventListener('click', addCardOpen(addCardInProgress, addCardInProgressDD, addCardItems, open));
 
-        if (open) {
-            addInProgress.classList.remove('add-card-active');
-            addCardInProgressDD.classList.remove('dropdown-active');
-
-            for (let i = 0; i < addCardItems.length; i++) {
-                addCardItems[i].classList.remove('shell-active');                
-            };
-            open = false;
-
-        } else {
-            addInProgress.classList.add('add-card-active');
-            addCardInProgressDD.classList.add('dropdown-active');
-
-            for (let i = 0; i < addCardItems.length; i++) {
-                addCardItems[i].classList.add('shell-active');                
-            };
-            open = true;
-            alert('1');
-        };
-    });
-
-    addInProgress.removeEventListener('click', addCardOpen(), true);
-
-    addInProgress.addEventListener('click', addCardClick => {
+    /*addCardInProgress.addEventListener('click', addCardClick => {
         alert('2');
         for (let i = 0; i < addCardItems.length; i++) {
             if (addCardClick.target.className === `li_${i}`) {
                 chooseAddCardItem(i);
             };
         };        
-    });
+    });*/
+}
+
+function addCardOpen(addCard, dropdown, arr, ddstate) {
+    if (ddstate) {
+        addCard.classList.remove('add-card-active');
+        dropdown.classList.remove('dropdown-active');
+
+        for (let i = 0; i < arr.length; i++) {
+            arr[i].classList.remove('shell-active');                
+        };
+        ddstate = false;
+    } else {
+        addCard.classList.add('add-card-active');
+        dropdown.classList.add('dropdown-active');
+        
+        for (let i = 0; i < arr.length; i++) {
+            arr[i].classList.add('shell-active');                
+        };
+        ddstate = true;
+        alert('1');
+    };
 }
